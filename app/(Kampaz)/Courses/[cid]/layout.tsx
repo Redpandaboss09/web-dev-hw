52407c1ccc9c041ae9375274b9d7de4851129ab1
Matthew <mattbrosgame.dev@gmail.com>
2025-10-17 23:34:34 -0400

    bugs

--- app/(Kampaz)/Courses/[cid]/layout.tsx
+++ app/(Kampaz)/Courses/[cid]/layout.tsx
@@ -1,18 +1,14 @@
-'use client';
-
-import { ReactNode } from "react";
+import ReactNode from "react";
+import LayoutProps from "next";
 import CourseNavigation from "./Navigation";
 import { FaAlignJustify } from "react-icons/fa";
 import { courses } from "../../Database";
 
-export default function CoursesLayout({
-                                          children,
-                                          params,
-                                      }: {
-    children: ReactNode;
-    params: { cid: string };
-}) {
-    const { cid } = params;
+export default async function CoursesLayout({
+                                                children,
+                                                params,
+                                            }: LayoutProps<"/Courses/[cid]">) {
+    const { cid } = await params;
     const course = courses.find((c) => c._id === cid);
 
     return (
@@ -22,14 +18,11 @@ export default function CoursesLayout({
                 {course?.name ?? "Course"}
             </h2>
             <hr />
-
             <div className="d-flex">
                 <aside className="d-none d-md-block me-3" style={{ minWidth: 220 }}>
                     <CourseNavigation cid={cid} />
                 </aside>
-                <main className="flex-fill">
-                    {children}
-                </main>
+                <main className="flex-fill">{children}</main>
             </div>
         </div>
     );
