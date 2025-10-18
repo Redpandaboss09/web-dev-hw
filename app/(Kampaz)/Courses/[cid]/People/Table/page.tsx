1abc99c51cf833c16a69e89c29c61bc97b9b9199
Matthew <mattbrosgame.dev@gmail.com>
2025-10-17 23:29:15 -0400

    bugs

--- app/(Kampaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kampaz)/Courses/[cid]/People/Table/page.tsx
@@ -6,6 +6,18 @@ import { FaUserCircle } from "react-icons/fa";
 import { useParams } from "next/navigation";
 import * as db from "../../../../Database";
 
+type User = {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    loginId: string;
+    section: string;
+    role: string;
+    lastActivity: string;
+    totalActivity: string;
+};
+
+
 export default function PeopleTable() {
     const { cid } = useParams();
     const { users, enrollments } = db;
@@ -21,7 +33,7 @@ export default function PeopleTable() {
                         .filter((usr) =>
                             enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid)
                         )
-                        .map((user: any) => (
+                        .map((user: User) => (
                             <tr key={user._id}>
                                 <td className="wd-full-name text-nowrap">
                                     <FaUserCircle className="me-2 fs-1 text-secondary" />
