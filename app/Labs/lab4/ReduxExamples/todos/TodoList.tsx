54f8bf2e4a7176b3122ae6049ca71ecd57559b24
Matthew <mattbrosgame.dev@gmail.com>
2025-11-04 00:34:02 -0500

    bugs

--- app/Labs/lab4/ReduxExamples/todos/TodoList.tsx
+++ app/Labs/lab4/ReduxExamples/todos/TodoList.tsx
@@ -1,22 +1,32 @@
-"use client"
-import { useState } from "react";
+"use client";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 import { useSelector } from "react-redux";
 import { Card, ListGroup } from "react-bootstrap";
+import type { RootState } from "../../store";
+
+interface Todo {
+    id: string;
+    title: string;
+}
 
 export default function TodoList() {
-    const { todos } = useSelector((state: any) => state.todosReducer);
+    const todos = useSelector<RootState, Todo[]>(
+        (state) => state.todosReducer.todos
+    );
+
     return (
         <div id="wd-todo-list-redux">
             <h2 className="mb-3">Todo List</h2>
             <Card className="shadow-sm">
                 <ListGroup variant="flush">
                     <TodoForm />
-                    {todos.map((todo: any) => (
-                        // eslint-disable-next-line react/jsx-key
-                        <TodoItem todo={todo} />
+                    {todos.map((todo) => (
+                        <TodoItem key={todo.id} todo={todo} />
                     ))}
                 </ListGroup>
             </Card>
-            <hr/></div>);}
+            <hr />
+        </div>
+    );
+}
