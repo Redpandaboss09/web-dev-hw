ea73ec7fd45e57e9913cb00c78086cbd1a6d67bd
Matthew <mattbrosgame.dev@gmail.com>
2025-11-04 00:00:34 -0500

    bugs

--- app/(Kampaz)/Courses/[cid]/layout.tsx
+++ app/(Kampaz)/Courses/[cid]/layout.tsx
@@ -5,7 +5,9 @@ import { useSelector } from "react-redux";
 import { useParams } from "next/navigation";
 import { FaAlignJustify } from "react-icons/fa";
 
-export default async function CoursesLayout({ children }: { children: ReactNode }) {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+export default function CoursesLayout({ children }: { // @ts-expect-error
+    children: ReactNode }) {
     const { cid } = useParams();
     const { courses } = useSelector((state: any) => state.coursesReducer);
     const course = courses.find((course: any) => course._id === cid);
