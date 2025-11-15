4a2696054b2db4e69ace91da4d7ff5801686ed87
Matthew <mattbrosgame.dev@gmail.com>
2025-11-15 02:08:25 -0500

    bugs

--- app/Labs/lab5/client.ts
+++ app/Labs/lab5/client.ts
@@ -4,7 +4,7 @@ export const fetchWelcomeMessage = async () => {
     const response = await axios.get(`${HTTP_SERVER}lab5/welcome`);
     return response.data;
 };
-const ASSIGNMENT_API = `${HTTP_SERVER}/lab5/assignment`;
+const ASSIGNMENT_API = `${HTTP_SERVER}lab5/assignment`;
 export const fetchAssignment = async () => {
     const response = await axios.get(`${ASSIGNMENT_API}`);
     return response.data;
@@ -13,7 +13,7 @@ export const updateTitle = async (title: string) => {
     const response = await axios.get(`${ASSIGNMENT_API}title/${title}`);
     return response.data;
 };
-const TODOS_API = `${HTTP_SERVER}/lab5/todos`;
+const TODOS_API = `${HTTP_SERVER}lab5/todos`;
 export const fetchTodos = async () => {
     const response = await axios.get(TODOS_API);
     return response.data;
