476c521f9323e2befb0456b535ec514df6cdf83a
Matthew <mattbrosgame.dev@gmail.com>
2025-11-15 01:47:50 -0500

    bugs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,20 +6,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+        },
+    },
+
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+        ],
+    },
 ];
 
 export default eslintConfig;
